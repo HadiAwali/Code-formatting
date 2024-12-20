ac05903c1a871ed19e4af8400efa122d736f28f9
Anonymous <98743369+HadiAwali@users.noreply.github.com>
2024-12-20 22:20:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById("formatBtn").addEventListener("click", () => {
+    const codeType = document.getElementById("codeType").value;
+    const inputCode = document.getElementById("inputCode").value;
+
+    let formattedCode;
+    try {
+        if (codeType === "html") {
+            formattedCode = prettier.format(inputCode, {
+                parser: "html",
+                plugins: prettierPlugins,
+            });
+        } else if (codeType === "css") {
+            formattedCode = prettier.format(inputCode, {
+                parser: "css",
+                plugins: prettierPlugins,
+            });
+        } else if (codeType === "javascript") {
+            formattedCode = prettier.format(inputCode, {
+                parser: "babel",
+                plugins: prettierPlugins,
+            });
+        }
+        document.getElementById("outputCode").value = formattedCode;
+    } catch (error) {
+        document.getElementById("outputCode").value =
+            "Error formatting code: " + error.message;
+    }
+});
