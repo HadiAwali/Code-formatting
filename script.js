0b7fc736e6b4b9f11a4c286cbdbd7fc5465f7009
Anonymous <98743369+HadiAwali@users.noreply.github.com>
2024-12-20 23:04:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
-document.getElementById("formatBtn").addEventListener("click", () => {
-    const codeType = document.getElementById("codeType").value;
-    const inputCode = document.getElementById("inputCode").value;
+document.addEventListener("DOMContentLoaded", () => {
+    const codeTypeSelect = document.getElementById("codeType");
+    const inputCodeTextarea = document.getElementById("inputCode");
+    const formatButton = document.getElementById("formatBtn");
+    const outputCodeTextarea = document.getElementById("outputCode");
 
-    let formattedCode;
+    // Enable interaction
+    codeTypeSelect.disabled = false;
+    inputCodeTextarea.disabled = false;
 
-    try {
-        if (codeType === "html") {
-            formattedCode = prettier.format(inputCode, {
-                parser: "html",
-                plugins: prettierPlugins,
-            });
-        } else if (codeType === "css") {
-            formattedCode = prettier.format(inputCode, {
-                parser: "css",
-                plugins: prettierPlugins,
-            });
-        } else if (codeType === "javascript") {
-            formattedCode = prettier.format(inputCode, {
-                parser: "babel",
-                plugins: prettierPlugins,
-            });
+    formatButton.addEventListener("click", () => {
+        const codeType = codeTypeSelect.value;
+        const inputCode = inputCodeTextarea.value;
+
+        try {
+            let formattedCode;
+            if (codeType === "html") {
+                formattedCode = prettier.format(inputCode, {
+                    parser: "html",
+                    plugins: prettierPlugins,
+                });
+            } else if (codeType === "css") {
+                formattedCode = prettier.format(inputCode, {
+                    parser: "css",
+                    plugins: prettierPlugins,
+                });
+            } else if (codeType === "javascript") {
+                formattedCode = prettier.format(inputCode, {
+                    parser: "babel",
+                    plugins: prettierPlugins,
+                });
+            }
+            outputCodeTextarea.value = formattedCode;
+        } catch (error) {
+            outputCodeTextarea.value = "Error formatting code: " + error.message;
         }
-
-        document.getElementById("outputCode").value = formattedCode;
-    } catch (error) {
-        document.getElementById("outputCode").value =
-            "Error formatting code: " + error.message;
-    }
+    });
 });
